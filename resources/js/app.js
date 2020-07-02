8d1982789e65c239f0cd282ce3643934319a725a
Hilmi <hilmi.azman@srglobal.com>
2020-07-02 13:32:13 +0800

    Add main layout vue page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 require('./bootstrap');
 import router from "./routes";
 import VueRouter from "vue-router";
+import Index from "./Index";
 
 window.Vue = require('vue');
 
@@ -11,5 +12,9 @@ Vue.use(VueRouter);
 
 const app = new Vue({
     el: '#app',
-    router
+    router,
+    components: {
+
+    	"index": Index
+    }
 });
