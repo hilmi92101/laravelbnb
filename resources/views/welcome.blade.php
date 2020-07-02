08f21bc5728a5a12c193b31b1b7cb9445e7e22d9
Hilmi <hilmi.azman@srglobal.com>
2020-07-02 16:18:45 +0800

    Styling at scss folder

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
 
         <title>LaravelBnb</title>
 
-        
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <script src="{{ asset('js/app.js') }}" defer></script>
         
     </head>
